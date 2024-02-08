633c7912b1bfe400f5f8597628ef74672cd1966f
vasylku <vasylkuvayskov@gmail.com>
2024-02-08 17:38:14 -0600

    test1, react working v1

--- open-gis-viewer/src/main.js
+++ open-gis-viewer/src/main.js
@@ -12,7 +12,15 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
+      preload: MAIN_WINDOW_WEBPACK_ENTRY,
+      contentSecurityPolicy:`
+      default-src 'self' https://tile.openstreetmap.org;
+      style-src 'self' 'unsafe-inline' http://localhost:3000/open-gis-viewer/node_modules/ol/ol.css;
+      img-src 'self' https://tile.openstreetmap.org;
+      base-uri 'self';
+      connect-src 'self';
+    `,
+
     },
   });
 
@@ -21,59 +29,32 @@ const createWindow = () => {
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
+
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-document.on('ready', createWindow);
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-document.on('window-all-closed', () => {
+app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
 
-import Map from 'ol/Map.js';
-import OSM from 'ol/source/OSM.js';
-import TileLayer from 'ol/layer/Tile.js';
-import View from 'ol/View.js';
-
-const map = new Map({
-  layers: [
-    new TileLayer({
-      source: new OSM(),
-    }),
-  ],
-  target: 'map',
-  view: new View({
-    center: [0, 0],
-    zoom: 2,
-  }),
-});
-
-app.getElementById('zoom-out').onclick = function () {
-  const view = map.getView();
-  const zoom = view.getZoom();
-  view.setZoom(zoom - 1);
-};
-
-app.getElementById('zoom-in').onclick = function () {
-  const view = map.getView();
-  const zoom = view.getZoom();
-  view.setZoom(zoom + 1);
-};
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
--- open-gis-viewer/src/preload.js
+++ open-gis-viewer/src/preload.js
@@ -1,2 +1,15 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const { contextBridge, ipcRenderer } = require('electron');
+
+
+contextBridge.exposeInMainWorld(
+    // Object containing the APIs to expose
+    'electron',
+    {
+        // Example API to send a message to the main process
+        send: (channel, data) => {
+            ipcRenderer.send(channel, data);
+        }
+    }
+);
