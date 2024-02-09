c9f98b1aecc033dd4f6192cb5a03ed0e6d40a487
JakeIsCodingAway <jake.s.held123@gmail.com>
2024-02-08 18:42:32 -0600

    i did a ting

--- open-gis-viewer/src/app.jsx
+++ open-gis-viewer/src/app.jsx
@@ -2,5 +2,10 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import Maps from "./components/Maps";
 
-const root = createRoot(document.body);
-root.render(<Maps/>);
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+    <React.StrictMode>
+        <Maps />
+    </React.StrictMode>,
+    rootElement
+);
