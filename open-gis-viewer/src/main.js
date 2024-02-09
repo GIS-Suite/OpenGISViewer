41221256dc48ef922d13803ce469e923d644743a
TrevorPatorno <trevor.patorno@selu.edu>
2024-02-08 19:39:26 -0600

    n

--- open-gis-viewer/src/main.js
+++ open-gis-viewer/src/main.js
@@ -1,8 +1,10 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+//Test 1
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
+if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
@@ -13,14 +15,13 @@ const createWindow = () => {
     height: 600,
     webPreferences: {
       preload: MAIN_WINDOW_WEBPACK_ENTRY,
-      contentSecurityPolicy:`
+      contentSecurityPolicy: `
       default-src 'self' https://tile.openstreetmap.org;
       style-src 'self' 'unsafe-inline' http://localhost:3000/open-gis-viewer/node_modules/ol/ol.css;
       img-src 'self' https://tile.openstreetmap.org;
       base-uri 'self';
       connect-src 'self';
     `,
-
     },
   });
 
@@ -29,32 +30,29 @@ const createWindow = () => {
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
-
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
     }
-  })
-})
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
-
-
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
