02184811b3432324a0b76164c13d3fa418225970
vasylku <vasylkuvayskov@gmail.com>
2024-02-15 01:20:38 -0600

    adding support for xyz wms

--- open-gis-viewer/src/components/Maps.js
+++ open-gis-viewer/src/components/Maps.js
@@ -1,20 +1,27 @@
 import React, {useEffect, useRef, useState} from 'react';
+import  "./ControlMenu.css";
 import 'ol/ol.css';
 import Map from 'ol/Map';
 import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
-import XYZ from 'ol/source/XYZ';
-import { MousePosition, defaults as defaultControls } from 'ol/control';
+import {MousePosition, defaults as defaultControls, ZoomSlider, Attribution} from 'ol/control';
 import { ScaleLine } from 'ol/control';
-import {TileWMS} from "ol/source";
+import TileLayer from "ol/layer/Tile";
+import XYZ from "ol/source/XYZ";
+import {ImageWMS, TileJSON, TileWMS, WMTS} from "ol/source";
+import ImageLayer from "ol/layer/Image";
+import {GeoJSON, WMTSCapabilities} from "ol/format";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
-
-function Maps({layerData, newLayer}) {
-    const [maps, setMaps] = useState();
+import {bbox as bboxStrategy} from 'ol/loadingstrategy.js';
+function Maps() {
+    const [maps, setMaps] = useState({});
     const mapElement = useRef();
-     console.log( newLayer);
+
+    const [layerType, setLayerType] = useState('XYZ');
+    const [layerUrl, setLayerUrl] = useState('');
+
+
 
     useEffect(() => {
 
@@ -22,9 +29,7 @@ function Maps({layerData, newLayer}) {
             target: mapElement.current,
             layers: [
                 new TileLayer({
-                    source: new XYZ({
-                        url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-                    })
+                    source: new OSM(),
                 })
             ],
             view: new View({
@@ -41,25 +46,102 @@ function Maps({layerData, newLayer}) {
                     target: document.getElementById('mouse-position'),
                     undefinedHTML: '&nbsp;'
                 }),
-                new ScaleLine()
+                new ScaleLine(),
+                new ZoomSlider(),
+                new ScaleLine(),
+
+
             ]),
 
         });
-        if (newLayer) {
-            initialMap.addLayer(newLayer);
+
+setMaps(initialMap);
+
+
+    }, []);
+    const handleAddLayer = () => {
+console.log("my map", maps);
+        let layerToAdd;
+
+        switch (layerType) {
+            case 'XYZ':
+                layerToAdd = new TileLayer({
+                    source: new XYZ({
+                        url: layerUrl,
+
+                    }),
+
+                });
+                break;
+            case 'WMS':
+                layerToAdd = new TileLayer({
+                    source: new TileWMS({
+                        url: layerUrl,
+                        params: {'LAYERS': 'topp:states', 'TILED': true},
+                        serverType: 'geoserver',
+                        transition: 0,
+                    })
+                });
+                break;
+            case 'WFS':
+                layerToAdd = new VectorLayer({
+                    source: new VectorSource({
+                        format: new GeoJSON(),
+                        url: function(extent) {
+                            return ( layerUrl +
+                                'version=1.1.0&request=GetFeature&typename=osm:water_areas&' +
+                                'outputFormat=application/json&srsname=EPSG:3857&' +
+                                'bbox=' +
+                                extent.join(',') +
+                                ',EPSG:3857'
+                        );
+                        },
+                        strategy: bboxStrategy,
+                    })
+                });
+                break;
+            case 'WMTS':
+                const attribution = new Attribution({
+                    collapsible: false,
+                });
+                const parser = new WMTSCapabilities();
+
+
+                break;
+            default:
+                console.error('Invalid layer type');
+                return;
         }
 
+        if (maps) {
+ maps.addLayer(layerToAdd);
+ console.log("maps: ", maps.getLayers());
+ setLayerUrl('');
 
-        return () => {
-            initialMap.setTarget(null);
-        };
-    }, [newLayer]);
-
+        }
+    };
     return (
         <>
             <div id='map' className="map"
                  ref={mapElement}/>
             <div id="mouse-position" className="mouse-position"/>
+            <div className='content' >
+
+                <select value={layerType} onChange={(e) => setLayerType(e.target.value)}>
+                    <option value="XYZ">XYZ (Tile) Layer</option>
+                    <option value="WFS">WFS  Layer</option>
+                    <option value="WMS">WMS  Layer</option>
+                    <option value="WMTS">WMTS  Layer</option>
+                </select>
+                <input
+                    type="text"
+                    value={layerUrl}
+                    onChange={(e) => setLayerUrl(e.target.value)}
+                    placeholder="Enter layer URL"
+                />
+                <button className="control-btn"
+                        onClick={handleAddLayer}>Add Layer</button>
+            </div>
 
         </>
 
