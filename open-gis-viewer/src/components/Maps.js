f740428e7f905829d9d8e5400d4560296d305c78
vasylku <vasylkuvayskov@gmail.com>
2024-02-16 01:05:10 -0600

    added bluemarble, wip wms

--- open-gis-viewer/src/components/Maps.js
+++ open-gis-viewer/src/components/Maps.js
@@ -4,86 +4,111 @@ import 'ol/ol.css';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import OSM from 'ol/source/OSM';
-import {MousePosition, defaults as defaultControls, ZoomSlider, Attribution} from 'ol/control';
+import {MousePosition, defaults as defaultControls, ZoomSlider, Attribution, OverviewMap} from 'ol/control';
 import { ScaleLine } from 'ol/control';
 import TileLayer from "ol/layer/Tile";
 import XYZ from "ol/source/XYZ";
 import {ImageWMS, TileJSON, TileWMS, WMTS} from "ol/source";
-import ImageLayer from "ol/layer/Image";
-import {GeoJSON, WMTSCapabilities} from "ol/format";
+import {GeoJSON, WMSCapabilities, WMTSCapabilities} from "ol/format";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
-import {bbox as bboxStrategy} from 'ol/loadingstrategy.js';
+import {bbox as bboxStrategy} from 'ol/loadingstrategy';
 function Maps() {
     const [maps, setMaps] = useState({});
     const mapElement = useRef();
-
     const [layerType, setLayerType] = useState('XYZ');
     const [layerUrl, setLayerUrl] = useState('');
 
+    //parser for WMS xml
+    const parser = new WMSCapabilities();
+    const fetchWms = async () => {//fetch bluemarble layer or else from url
+        try {
+            const response = await fetch('https://geoint.nrlssc.org/nrltileserver/wms?REQUEST=GetCapabilities&SERVICE=WMS&VERSION=1.3.0');
+            const text = await response.text();
+            const result = parser.read(text);
+            const marble = result.Capability.Layer.Layer.find(layer => layer.Name === 'bluemarble');
+            return marble;
 
+        } catch (error) {
+            console.error('Error fetching or parsing WMS capabilities:', error);
+        }
+    };
 
-    useEffect(() => {
+    useEffect( () => {
+        //initialize a  main Map
+        const initMap = async () => {
+            let res = await fetchWms();
+            console.log("marble;", res);
+            const initialMap = new Map({
 
-        const initialMap = new Map({
-            target: mapElement.current,
-            layers: [
-                new TileLayer({
-                    source: new OSM(),
-                })
-            ],
-            view: new View({
-                center: [0, 0],
-                zoom: 2
-            }),
-            controls: defaultControls().extend([
-                new MousePosition({
-                    coordinateFormat: (coordinate) => {
-                        return `Coordinates: ${coordinate[0].toFixed(2)}, ${coordinate[1].toFixed(2)}`;
-                    },
-                    projection: 'EPSG:4326',
-                    className: 'custom-mouse-position',
-                    target: document.getElementById('mouse-position'),
-                    undefinedHTML: '&nbsp;'
+                target: mapElement.current,
+                layers: [// core layer
+                    new TileLayer({
+                        source: new TileWMS({
+                            url: 'https://geoint.nrlssc.org/nrltileserver/wms',
+                            params: {
+                                'LAYERS': res.Name,
+                            },
+                            serverType: 'geoserver',
+                        }),
+                    }),
+                ],
+                view: new View({
+                    center: [0, 0],
+                    zoom: 2,
                 }),
-                new ScaleLine(),
-                new ZoomSlider(),
-                new ScaleLine(),
+                //extra controls for the map
+                controls: defaultControls().extend([
+                    new MousePosition({
+                        coordinateFormat: (coordinate) => {
+                            return `Coordinates: ${coordinate[0].toFixed(2)}, ${coordinate[1].toFixed(2)}`;
+                        },
+                        projection: 'EPSG:4326',
+                        className: 'cursor-map-controls',
+                        //  target: document.getElementById('mouse-position'),
 
+                        undefinedHTML: '&nbsp;'
+                    }),
+                    new ScaleLine({units: 'us'}),
+                    new ZoomSlider(),
+                    new OverviewMap({
+                        layers: [
 
-            ]),
-
-        });
-
-setMaps(initialMap);
-
-
+                            new TileLayer({
+                                source: new OSM(),
+                            })
+                        ],
+                        rotateWithView: true,
+                    }),
+                ]),
+            });
+            setMaps(initialMap);
+        }
+        initMap();
     }, []);
+    //handle adding layers based on user input
     const handleAddLayer = () => {
-console.log("my map", maps);
+        console.log("Main map", maps);
         let layerToAdd;
 
-        switch (layerType) {
+        switch (layerType) {//support XYZ here
             case 'XYZ':
                 layerToAdd = new TileLayer({
                     source: new XYZ({
                         url: layerUrl,
 
                     }),
-
                 });
                 break;
-            case 'WMS':
+            case 'WMS'://add WMS layeres suport, TileWMS , todos ImageWMS
                 layerToAdd = new TileLayer({
                     source: new TileWMS({
                         url: layerUrl,
-                        params: {'LAYERS': 'topp:states', 'TILED': true},
                         serverType: 'geoserver',
-                        transition: 0,
                     })
                 });
                 break;
-            case 'WFS':
+            case 'WFS':// suport for WFS TODOS
                 layerToAdd = new VectorLayer({
                     source: new VectorSource({
                         format: new GeoJSON(),
@@ -94,13 +119,13 @@ console.log("my map", maps);
                                 'bbox=' +
                                 extent.join(',') +
                                 ',EPSG:3857'
-                        );
+                            );
                         },
                         strategy: bboxStrategy,
                     })
                 });
                 break;
-            case 'WMTS':
+            case 'WMTS'://support for WMTS
                 const attribution = new Attribution({
                     collapsible: false,
                 });
@@ -112,26 +137,26 @@ console.log("my map", maps);
                 console.error('Invalid layer type');
                 return;
         }
-
         if (maps) {
- maps.addLayer(layerToAdd);
- console.log("maps: ", maps.getLayers());
- setLayerUrl('');
-
+            //add layers to main map
+            maps.addLayer(layerToAdd);
+            console.log("maps: ", maps.getLayers());
+            setLayerUrl('');
         }
     };
+
     return (
         <>
             <div id='map' className="map"
                  ref={mapElement}/>
             <div id="mouse-position" className="mouse-position"/>
-            <div className='content' >
+            <div className='content'>
 
                 <select value={layerType} onChange={(e) => setLayerType(e.target.value)}>
                     <option value="XYZ">XYZ (Tile) Layer</option>
-                    <option value="WFS">WFS  Layer</option>
-                    <option value="WMS">WMS  Layer</option>
-                    <option value="WMTS">WMTS  Layer</option>
+                    <option value="WFS">WFS Layer</option>
+                    <option value="WMS">WMS Layer</option>
+                    <option value="WMTS">WMTS Layer</option>
                 </select>
                 <input
                     type="text"
@@ -140,12 +165,10 @@ console.log("my map", maps);
                     placeholder="Enter layer URL"
                 />
                 <button className="control-btn"
-                        onClick={handleAddLayer}>Add Layer</button>
+                        onClick={handleAddLayer}>Add Layer
+                </button>
             </div>
-
         </>
-
-
     );
 }
 
