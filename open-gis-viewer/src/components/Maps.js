6527646204b36ba9b89a9a7334a285c06bd2ed6d
vasylku <vasylkuvayskov@gmail.com>
2024-03-19 01:33:30 -0500

    wip geotiffs and filter input data

--- open-gis-viewer/src/components/Maps.js
+++ open-gis-viewer/src/components/Maps.js
@@ -14,16 +14,6 @@ function Maps() {
     const [maps, setMaps] = useState({});
     const mapElement = useRef();
     const [expanded, setExpanded] = useState(false);
-
-    function redraw(l) {
-        const layers = [...l];
-        //maps.getLayers().clear();
-        maps.setLayers(layers);
-        maps.getLayers().forEach(layer => {
-            layer.getSource().refresh();
-        });
-    }
-
     const toggleBottomBar = () => {
         setExpanded(!expanded);
     };
@@ -88,9 +78,6 @@ function Maps() {
         <>
             <div id='map' className="map" ref={mapElement}/>
             <button className="menu-btn" onClick={toggleBottomBar}>{expanded ? "Hide" : "Map"} </button>
-            <button className="redraw-btn" onClick={() => redraw(maps.getLayers().getArray())
-            }></button>
-            {/*<button className="search-btn" onClick={toggleSearchUrl}>{searching ? "Hide" : "Import"}</button>*/}
             <div className={`bottom-container ${expanded ? 'bottom-expanded' : ''}`}>
                 <MapInfo map={maps} onToogleBottomMenu={toggleBottomBar}/>
             </div>
